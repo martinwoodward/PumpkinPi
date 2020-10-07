09b4dca4153aaadb3dedf1af3b86c3ba2cea5aee
Martin Woodward <martinwoodward@github.com>
2020-10-07 13:27:39 +0100

    Test flame colors

--- src/flametest.py
+++ src/flametest.py
@@ -18,9 +18,13 @@ def flame(hue, duration):
         rand_mat = numpy.random.rand(width,height)
         for y in range(height):
             for x in range(width):
+                # h is the percentage of the 360 degrees of hue that we start from
+                # then we add a random amount to each pixel to a max of 10%
+                # so 30 h in the traditional hsv space
                 h = hue + (0.1 * rand_mat[x, y])
-                s = 0.8
+                s = 1
                 v = rand_mat[x, y]
+                # Convert to RGB to send to the LED pixels
                 rgb = colorsys.hsv_to_rgb(h, s, v)
                 r = int(rgb[0]*255.0)
                 g = int(rgb[1]*255.0)
@@ -36,10 +40,12 @@ unicorn.rotation(0)
 unicorn.brightness(0.5)
 width,height=unicorn.get_shape()
 
-flame(0.0, 2)   # Red
-flame(0.1, 2)   # Yellow
-flame(0.2, 2)   # Green
-flame(0.4, 2)   # Light Blue
-flame(0.8, 2)   # Purple
+flame(0.9, 5)   # Red
+flame(0.0, 5)   # Orange
+flame(0.1, 5)   # Yellow
+flame(0.2, 5)   # Green
+flame(0.4, 5)   # Light Blue
+flame(0.5, 5)   # Blue
+flame(0.8, 5)   # Purple
 
 
